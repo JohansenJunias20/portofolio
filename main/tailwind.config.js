240c6a57b74045a4fb3db39fd6fca6f5733c14b8
JohansenJunias20 <johansen.gumbal@gmail.com>
2021-03-29 23:39:48 +0700

    build tailwind, init html, adding resources

---
+++ main/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': "#FFC414",
+        'txt': "#FFF5CC",
+        'redbg': "#C20101",
+        'secondary': "#F552CF"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
