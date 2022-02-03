eb9eed6be86d10ea75a7de4036aa2e3b19059f81
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-02-04 00:03:19 +0700

    notes to bash script

--- dev.sh
+++ dev.sh
@@ -7,4 +7,7 @@ echo "please use live server extension to serve index.html file (relative path t
 export $(cat .env | xargs)
 chmod 777 ./private/main.exe;
 ./private/main.exe $DEV_WS_PORT $PROD_WS_DOMAIN;
+echo "";
+echo "IMPORTANT!!! PLEASE IGNORE ALL ERRORS CAUSED BY WEBPACK COMPILER";
+sleep 5;
 bash ./build.sh -m DEV & docker-compose -f docker-compose.dev.yml up;
