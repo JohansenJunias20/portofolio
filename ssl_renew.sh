b0c5ac46648ecf5f21bd7465fd967054995e3804
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-02-03 22:39:47 +0700

    adding ssl folder

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,11 +4,11 @@ echo "generating ssh if expired. please make sure port 80 is open and not used b
 
 export $(cat .env | xargs)
 echo "generating ssl for websocket server..."
-sudo docker run -it --rm --name certbot -p 80:80 -v "/$(pwd)/ssl/main:/etc/letsencrypt" -v "/$(pwd)/ssl/lib:/var/lib/letsencrypt" \
+docker run -it --rm --name certbot -p 80:80 -v "/$(pwd)/ssl/main:/etc/letsencrypt" -v "/$(pwd)/ssl/lib:/var/lib/letsencrypt" \
 certbot/certbot \
 certonly --standalone --email johansen.gumbal@gmail.com --agree-tos --no-eff-email -d $TURN_DOMAIN --keep-until-expiring
 
 echo "generating ssl for websocket production server..."
-sudo docker run -it --rm --name certbot -p 80:80 -v "/$(pwd)/ssl/main:/etc/letsencrypt" -v "/$(pwd)/ssl/lib:/var/lib/letsencrypt" \
+docker run -it --rm --name certbot -p 80:80 -v "/$(pwd)/ssl/main:/etc/letsencrypt" -v "/$(pwd)/ssl/lib:/var/lib/letsencrypt" \
 certbot/certbot \
 certonly --standalone --email johansen.gumbal@gmail.com --agree-tos --no-eff-email -d $PROD_WS_DOMAIN --keep-until-expiring
