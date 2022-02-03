6dad990d76e44f10784a4bb398352d39fe0cba17
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-02-03 22:30:40 +0700

    ssl must be included otherwise ws server production mode cannot run

--- dev.sh
+++ dev.sh
@@ -5,6 +5,6 @@ echo "" >> ws-server/.env;
 echo "" >> ws-server/.env;
 echo "please use live server extension to serve index.html file (relative path to public file)"
 export $(cat .env | xargs)
-sudo chmod 777 ./private/main.exe;
-sudo ./private/main.exe $DEV_WS_PORT;
-bash ./build.sh -m DEV & docker-compose -f docker-compose.dev.yml up;
+chmod 777 ./private/main.exe;
+./private/main.exe $DEV_WS_PORT;
+# bash ./build.sh -m DEV & docker-compose -f docker-compose.dev.yml up;
