a0ce169b1a6fdb617471f5d29d30b43596ee3a1c
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-02-04 10:34:55 +0700

    adding date

--- ws-server/index.js
+++ ws-server/index.js
@@ -28,11 +28,40 @@ function noteVisitor(req, res) {
     var visitor = []
     if (fs.existsSync("./visitors/visitor.json"))
         visitor = JSON.parse(fs.readFileSync('./visitors/visitor.json'))
+    body.time = getDateNow();
     visitor.push(body)
     fs.writeFileSync('./visitors/visitor.json', JSON.stringify(visitor));
 
 }
 const IDs = [];
+function getDateNow() {
+    let date_ob = new Date();
+
+    // current date
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    // current hours
+    let hours = date_ob.getHours();
+
+    // current minutes
+    let minutes = date_ob.getMinutes();
+
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+    // prints date in YYYY-MM-DD format
+    console.log(year + "-" + month + "-" + date);
+
+    // prints date & time in YYYY-MM-DD HH:MM:SS format
+    return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+}
 io.on("connection", (socket) => {
     socket.on("offer", (e) => {
         const { id, sdp } = e;
