1484d21981930b4f72be7aae0eb907f9a7e33e6c
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-02-09 00:12:29 +0700

    now prod only build files

--- build.sh
+++ build.sh
@@ -84,8 +84,8 @@ if [ "$DOCKER" == "true" ]; then
     echo "> COMPILING TYPESCRIPT..."
     if [ "$MODE" == "PROD" ]; then
         # build dist js
-        docker run -p 8081:8080 -v "/$(pwd)/:/usr/src/app" customnode:latest \
-        npx webpack-dev-server --config webpack.prod.js \
+        docker run  -v "/$(pwd)/:/usr/src/app" customnode:latest \
+        npx webpack --config webpack.prod.js \
         --env=TURN_DOMAIN=$TURN_DOMAIN --env=WEBSOCKET_DOMAIN=$PROD_WS_DOMAIN \
         --env=TURN_USERNAME=$TURN_USERNAME --env=TURN_PASSWORD=$TURN_PASSWORD --env=WEBSOCKET_PORT=$PROD_WS_PORT \
         --env=TURN_PORT=$TURN_PORT --env=TURN_PORT_TLS=$TURN_PORT_TLS --env=TURN_MIN_PORT=$TURN_MIN_PORT --env=TURN_MAX_PORT=$TURN_MAX_PORT
