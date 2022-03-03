16dc0baaf2ae0e5fc5c5378714616847aec3649c
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-03-03 14:52:38 +0700

    tested on prod server

--- coturn_docker.sh
+++ coturn_docker.sh
@@ -1,5 +1,4 @@
-docker run -d -p 3478:3478 -p 49160-49200:49160-49200/udp \
-    -v "/$(pwd)/turnserver.conf:/etc/coturn/turnserver.conf" \
-    -v "/$(pwd)/ssl/main/:/etc/letsencrypt/" \
-    instrumentisto/coturn -n --log-file=stdout \
-    --min-port=49160 --max-port=49200
+docker run -p 3478:3478 -p 5349:5349 -p 49120-49200:49120-49200/udp \
+    -v "$(pwd)/turnserver.conf:/etc/coturn/turnserver.conf" \
+    -v "$(pwd)/ssl/main/:/etc/letsencrypt/" \
+    instrumentisto/coturn 
