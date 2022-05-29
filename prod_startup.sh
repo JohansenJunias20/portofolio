cbf31d5787404602cfa3d8474543b807c6bb87c2
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-05-29 23:29:04 +0700

    avoid conflic port on renew ssl certbot, add sleep for 60 seconds waiting renew ssl on rpok.service on boot

--- prod_startup.sh
+++ prod_startup.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-export $(cat .env | xargs)
-
-# renew ssl
-bash ./ssl_renew.sh
-
-# compile typescript to dist file
-# installing dependencies
-docker run -v "/$(pwd)/ws-server:/usr/src/app" customnode:latest npm install
-
-# copy .env file to ws-server/
-cp .env ws-server/.env
-
-echo "RUNNING WEBSOCKET SERVER..."
-echo "" >>ws-server/.env
-echo "PRODUCTION=TRUE" >>ws-server/.env
-# start socketio server
-bash config_turn.sh; # configuration turnserver.conf
-docker-compose -f docker-compose.prod.yml down;
-docker-compose -f docker-compose.prod.yml up -d;
-bash ./build.sh -m PROD -d;
-
+sleep 60; # avoid conflic renew ssl with rpok.service(reverse-proxy-orbits-komputer) when system boot up.
+bash prod.sh
