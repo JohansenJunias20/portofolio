9c6b8985c69d66ac3384b1f27056bffb7dc7c3bd
JohansenJunias20 <johansen.gumbal@gmail.com>
2022-06-14 22:49:53 +0700

    wrong path dockerfile

--- prod.sh
+++ prod.sh
@@ -4,7 +4,6 @@ export $(cat .env | xargs)
 bash ./ssl_renew.sh
 
 # docker build Dockerfile to customnode image
-docker build -t customnode .
 # compile typescript to dist file
 # installing dependencies
 docker run -v "/$(pwd)/ws-server:/usr/src/app" customnode:latest npm install
